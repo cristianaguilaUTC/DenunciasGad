8b0f6b37580ec8e0f3159be25acb44b26f2a2f78
hanna2424 <armijosalejandra2001@gmail.com>
2025-06-29 17:28:06 -0500

    creando el models.py de funcionarios

--- Aplicaciones/Funcionarios/models.py
+++ Aplicaciones/Funcionarios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Funcionario(models.Model):
+    id=models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    cargo = models.CharField(max_length=100)
+
+    def _str_(self):
+        fila="{0}: {1} {2} {3} "
+        return fila.format(self.id, self.nombre, self.correo, self.cargo)
