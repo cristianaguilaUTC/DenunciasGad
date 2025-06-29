6c3c4a5ad227398f92a55857cbce733cdd18604c
hanna2424 <armijosalejandra2001@gmail.com>
2025-06-29 17:27:34 -0500

    creando el models.py de usuarios

--- Aplicaciones/Usuarios/models.py
+++ Aplicaciones/Usuarios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    id=models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    contrasena = models.CharField(max_length=128)
+    tipo_usuario = models.CharField(max_length=50)
+    anonimo = models.BooleanField(default=False)
+
+    def _str_(self):
+        fila="{0}: {1} {2} {3} {4} "
+        return fila.format(self.id, self.nombre, self.correo, self.contrasena, self.tipo_usuario, self.anonimo)
