98ff8a7bfc26c9594b8658ce40960b1f33b77568
hanna2424 <armijosalejandra2001@gmail.com>
2025-06-29 17:37:14 -0500

    creando el models.py dedenuncias

--- Aplicaciones/Denuncias/models.py
+++ Aplicaciones/Denuncias/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from Aplicaciones.Usuarios.models import Usuario
 
 # Create your models here.
+class Denuncia(models.Model):
+    id = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    ubicacion = models.CharField(max_length=255)
+    latitud = models.FloatField()
+    longitud = models.FloatField()
+    estado = models.CharField(max_length=50)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        fila="{0}: {1} {2} {3} {4} {5} {6} {7} {8}"
+        return fila.format(self.id, self.usuario, self.titulo, self.descripcion, self.ubicacion, self.latitud, self.longitud, self.estado, self.fecha_creacion)
